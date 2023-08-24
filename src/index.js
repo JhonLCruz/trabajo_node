b2f4fddeceebd55718697e97e9b5e436c54ab060
kevin huelgos <kahuelgos@misena.edu.co>
2023-08-23 20:58:57 -0500

    terminada la conexion

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import expresss from express
+import express from 'express';
+import {dirname, join} from 'path';
+import { fileURLToPath } from "url";
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.set('views', join(__dirname, 'views'))
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('index'))
+console.log(join(__dirname, 'views'))
+
+app.listen(3000)
