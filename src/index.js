e4d33ade4f91521b57e5adc056977335ae8f0a9f
kevin huelgos <kahuelgos@misena.edu.co>
2023-08-23 22:27:02 -0500

    ya casi

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import {dirname, join} from 'path';
 import { fileURLToPath } from "url";
+import indexRoutes from './routes/index.js'
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url))
-
 app.set('views', join(__dirname, 'views'))
-
 app.set('view engine', 'ejs')
+app.use()
 
-app.get('/', (req, res) => res.render('index'))
-console.log(join(__dirname, 'views'))
+
 
 app.listen(3000)
